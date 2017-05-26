45aa063409c89a4ced10599c50dce709c7a0489f
americas\henriqgu <gustavo.braghetti@hpe.com>
2017-05-26 11:43:53 -0300

    Projeto remover imagens Docker do Jenkins

---
+++ br/com/dxc/business/RemoverImage.py
@@ -0,0 +1,77 @@
+import docker
+#from br.com.dxc.teste.DockerFake import DockerFake
+
+# import pdb; pdb.set_trace() --> modo interativo de DEBUG
+
+
+# client = docker.DockerClient(base_url="tcp://192.168.99.100:2376")
+
+'''
+Imagens reais alocadas no Vagrant'''
+
+client = docker.DockerClient(base_url='unix://var/run/docker.sock')
+imagesDocker = client.images
+
+
+def deleteNones():
+    print("###### deletar nones #########")
+    #for image in DockerFake.list():
+    for image in imagesDocker.list():
+        if '<none>:<none>' in image.attrs["RepoTags"]:
+            #DockerFake.remove(image.short_id, force=True)
+            imagesDocker.remove(image.short_id, force=True)
+            print(image.attrs["RepoTags"])
+
+
+listNomes = []
+def addInList():
+    #for img in DockerFake.list():
+    for img in imagesDocker.list():
+        listNomes.append(img.attrs["RepoTags"].__str__())
+    print("*****************")
+
+listNameFormated = []
+def formatNameImg():
+    nomeAdd = []
+    for i in range(len(listNomes)):
+        nomeSplit = listNomes[i].split(":")
+        for nm in nomeSplit:
+            nomeAdd.append(nm)
+            listNameFormated.append(nomeAdd[0].replace("[u'", ""))
+            break
+        nomeAdd = []
+    return listNameFormated
+
+
+listData = []
+def searchName(listFormatNameImg):
+    for i in range(len(listFormatNameImg)):
+        #listImagensDebian = DockerFake.list(listFormatNameImg[i])
+        listImagensDebian = imagesDocker.list(listFormatNameImg[i])
+        for img in listImagensDebian:
+            print("######## IMAGENS")
+            print(img)
+            listData.append(([img.attrs["Created"]], [img]))
+        listData.sort(reverse=True)
+        #print("Permanecera: ")
+        #print(listData[0])
+        #deleteAll(listData)
+
+def deleteAll(plistData):
+    if len(plistData) > 1:
+        for i in range(1, len(plistData)):
+            print("Removendo %d %s" % (i, plistData[i]))
+
+
+########
+#addInList()
+#print(listNomes)
+#print(formatNameImg())
+#searchName(formatNameImg())
+#print(imagesDocker.list("amov/nginx"))
+#print("######## ALL")
+
+for img in imagesDocker.list():
+    print(img.attrs["Created"])
+
+
---
+++ br/com/dxc/teste/DockerFake.py
@@ -0,0 +1,36 @@
+import random
+
+class DockerFake(object):
+    repotags = None
+
+    def __init__(self, repotags, short_id, created):
+        self.repotags = repotags
+        self.short_id = short_id
+        self.created = created
+
+    '''
+        Fake para meus Dockers
+        def __init__(self, repotags, created):
+        self.short_id = self.getShort_id()
+        self.repotags = repotags
+        self.created = created
+    '''
+
+    def getShort_id(self):
+        return str(random.randrange(100000, 999999))
+
+    def setRepotags(self, repotags):
+        self.repotags = repotags
+
+    def getRepotags(self):
+        return self.repotags
+
+    def setCreated(self, created):
+        self.created = created
+
+    def getCreated(self):
+        return self.create
+
+    def __str__(self):
+        return "short_id = %s /repotags = %s /created = %s" %(self.short_id, self.repotags, self.created)
+
